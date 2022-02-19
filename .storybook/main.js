c5f458226e65f95759699f8756d0fa2ad741138a
tsukaryu <drgon8otsuka@gmail.com>
2022-02-20 00:43:33 +0900

    feat: first component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,17 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
   ],
   features: {
     postcss: false,
   },
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "webpack4",
+    builder: 'webpack4',
   },
 };
